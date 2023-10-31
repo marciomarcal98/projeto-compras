92790e5bd4def25799d787ed4a93eb52380fbd92
MarçalJS <marciomarcal.dev@gmail.com>
2023-10-31 20:44:11 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const exphbs = require("express-handlebars")
+const mysql = require("mysql")
+
+const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.use(express.static("public"))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000!")
+})
